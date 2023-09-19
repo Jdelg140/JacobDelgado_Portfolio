bc9777fb78f52199ae129d745a7c6a267ab4aca4
jdelg140 <jacobdelgado140@gmail.com>
2023-09-18 22:10:02 -0400

    added event share project to portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -95,15 +95,15 @@ function Projects() {
           <div className=" bg-gray-500 shadow-sm shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto">
             <div className="opacity-50 group-hover:opacity-100 hover:smooth">
               <span className="text-2xl font-bold text-white tracking-wider">
-                ReadMe Generator
+                Event Share
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://event-share123-cc0d9b418402.herokuapp.com/">
                   <button className=" opacity-0 group-hover:opacity-100 text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/juanx198522/Event-Share">
                   <button className=" opacity-0 group-hover:opacity-100 text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
